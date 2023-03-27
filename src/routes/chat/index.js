09b946ac9cbc908affce3f31c66e2e8644b5d5a3
Lautallarico <lauta.tallarico@gmail.com>
2023-03-27 20:28:21 -0300

    Retoques finales

--- src/routes/chat/index.js
+++ src/routes/chat/index.js
@@ -5,11 +5,9 @@ const router = Router()
 
 router.post('/', ChatController.saveChat)
 router.post('/:chatId/messages', ChatController.updatedChatById)
-// router.delete('/:cartId', CartController.deleteCart)
-// router.delete('/:cartId/products/:id_prod', CartController.deleteProductFromCart)
 router.get('/:chatId/messages', ChatController.messagesInChat)
 router.get('/:id', ChatController.chatById)
-// router.get('/:id/buyCart', CartController.buyCart)
+
 
 
 export { router as ChatRouter }
